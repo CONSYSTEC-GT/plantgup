eb426cc2a9ed3ca88610ab8e48480e579dddaf5f
Javier Colocho <javier.colocho@gmail.com>
2025-02-18 07:47:18 -0600

    guardo el token en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 import { useState, useMemo, useEffect } from 'react';
 import { BrowserRouter, useNavigate } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import { jwtDecode } from 'jwt-decode';
 import { Box } from '@mui/material';
 import AppRoutes from './routes';
@@ -9,6 +10,9 @@ import LoadingSpinner from './utils/LoadingSpinner';
 import LoginRequired from './pages/LoginRequired';
 
 function App() {
+
+  const location = useLocation();
+
   const [themeSettings, setThemeSettings] = useState({
     primaryColor: '#00C3FF',
     secondaryColor: '#DBDBDB',
@@ -19,39 +23,17 @@ function App() {
   const [tokenValid, setTokenValid] = useState(false);
 
   useEffect(() => {
-    const validateToken = async () => {
-      try {
-        const storedToken = localStorage.getItem('authToken');
+    const params = new URLSearchParams(location.search);
+    const token = params.get("token");
 
-        if (!storedToken) {
-          console.error('⚠️ No hay token almacenado');
-          setTokenValid(false);
-          return;
-        }
 
-        const decoded = jwtDecode(storedToken);
-        const currentTime = Date.now() / 1000;
-
-        if (decoded.exp < currentTime) {
-          console.error('⚠️ Token expirado');
-          localStorage.removeItem('authToken');
-          setTokenValid(false);
-          return;
-        }
-
-        console.log('✅ Token válido');
-        setTokenValid(true);
-      } catch (error) {
-        console.error('⚠️ Token inválido:', error);
-        localStorage.removeItem('authToken');
-        setTokenValid(false);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    validateToken();
-  }, []);
+    if (token) {
+      console.log("Guardando token en localStorage...");
+      localStorage.setItem("token", token);
+      // Opcional: Remover el token de la URL para limpiar la barra de direcciones
+      window.history.replaceState({}, document.title, window.location.pathname);
+    }
+  }, [location]);
 
   const theme = useMemo(() =>
     createTheme({
