a9218d3eb1dc2632442d44059b04abebd3680d8d
Javier Colocho <javier.colocho@gmail.com>
2025-02-19 07:45:31 -0600

    debug en upload file

--- src/components/FileUploadComponent.jsx
+++ src/components/FileUploadComponent.jsx
@@ -85,7 +85,7 @@ const FileUploadComponent = ({ templateType = 'media', onUploadSuccess }) => {
     };
 
     //const url = `/gupshup/partner/app/${APP_ID}/upload/media`;
-    const url = `https://api.gupshup.io/gupshup/partner/app/{{APP_ID}}/upload/media`;
+    const url = `https://api.gupshup.io/gupshup/partner/app/f63360ab-87b0-44da-9790-63a0d524f9dd/upload/media`;
     //https://partner.gupshup.io/partner/app/{{APP_ID}}/media
 
     // Debug: Imprimir el request completo
