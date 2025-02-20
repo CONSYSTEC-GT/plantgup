b46fd0ac1502e435d9284ed89dfac1d0451ace3c
Javier Colocho <javier.colocho@gmail.com>
2025-02-20 14:26:21 -0600

    prueba backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { mysqlHandler } from './functions/mysql-handler/resource';
+import { myFirstFunction } from './my-first-function/resource';
 
 // Define el backend con los recursos principales
 const backend = defineBackend({
   auth,
   data,
-  mysqlHandler,
+  myFirstFunction,
 });
