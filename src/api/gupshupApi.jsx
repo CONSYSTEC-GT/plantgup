7cbb101641524a92046e516d3d14ddde656686e7
javiercolochoConsystec <javier.colocho@consystec-corp.com>
2025-04-22 20:11:33 -0600

    FEATURE
    PUT REQUEST PARA EDITAR CAROUSEL

--- src/api/gupshupApi.jsx
+++ src/api/gupshupApi.jsx
@@ -260,3 +260,80 @@ export const createTemplateCarouselGupshup = async (appId, authCode, templateDat
     return null; // Retornar null en caso de error
   }
 };  
+
+export const editTemplateCarouselGupshup = async (appId, authCode, templateData, validateFn) => {
+  // Validar campos antes de enviar la solicitud
+  if (validateFn && !validateFn()) {
+    return null; // Detener la ejecución si hay errores
+  }
+
+  const {
+    templateName,
+    selectedCategory,
+    languageCode,
+    templateType,
+    vertical,
+    message,
+    example,
+    carousel
+  } = templateData;
+
+  const url = `https://partner.gupshup.io/partner/app/${appId}/templates`;
+  const headers = {
+    Authorization: authCode,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  const data = new URLSearchParams();
+  data.append("elementName", templateName);
+  data.append("category", selectedCategory.toUpperCase());
+  data.append("languageCode", languageCode);
+  data.append("templateType", templateType.toUpperCase());
+  data.append("vertical", vertical);
+  data.append("content", message);
+  data.append("example", example);
+  data.append("enableSample", true);
+  data.append("allowTemplateCategoryChange", false);
+  data.append("cards", carousel)
+
+  console.log("Request enviado:", JSON.stringify(Object.fromEntries(data.entries()), null, 2));
+
+  try {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: headers,
+      body: data,
+    });
+
+    console.log("Status code:", response.status);
+    console.log("Response headers:", Object.fromEntries([...response.headers.entries()]));
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      let errorResponse;
+      try {
+        errorResponse = JSON.parse(errorText);
+        console.error("Error response (JSON):", errorResponse);
+      } catch (e) {
+        errorResponse = { message: "Error no JSON", raw: errorText };
+        console.error("Error response (texto):", errorText);
+      }
+      showSnackbar(`❌ Error al crear la plantilla: ${errorResponse.message || "Solicitud inválida"}`, "error");
+      throw new Error(errorResponse.message || "Error al crear la plantilla");
+    }
+
+    const result = await response.json();
+    showSnackbar("✅ Plantilla creada exitosamente", "success");
+    console.log("Response: ", result);
+    return result; // Retornar el resultado
+  } catch (error) {
+    console.error("Error en la solicitud:", error);
+    console.error("Error detallado:", {
+      name: error.name,
+      message: error.message,
+      stack: error.stack
+    });
+    showSnackbar("❌ Error al crear la plantilla", "error");
+    return null; // Retornar null en caso de error
+  }
+};  
